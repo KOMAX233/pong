001f263262f4e9bdf7e620fcec3476e059061ff8
KOMAX233 <55835642+YueFangOfficial@users.noreply.github.com>
2021-09-30 15:01:12 -0400

    Add files via upload

--- game.cc
+++ game.cc
@@ -1,7 +1,18 @@
 #include "game.h"
 
 Game::Game(int w, int h) {
-    
+    srand(time(NULL));//! use current time as seed for random generator
+    quit = false;
+    up1 = "w";
+    down1 = "s";
+    up2 = "i"; // want to change it to up and down keys
+    down2 = "k";
+    score1 = score2 = 0;
+    width = w;
+    height = h;
+    ball1 = new Ball(w/2, h/2);
+    player1 = new Paddle(1, h/2 - 3);//!
+    player2 = new Paddle(w-2, h/2 - 3);//!
 }
 
 Game::~Game() {
@@ -14,4 +25,8 @@ void Game::scoreUp(Paddle * player) {
     } else {
         score2++;
     }
+
+    ball->reset();
+    //player1->reset();
+    //player2->reset();
 }
